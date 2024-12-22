71cd081fd323c2fe1c23d7872585c688fb47f64f
EGE <egebalyali@gmail.com>
2024-12-22 22:25:19 +0300

    Webconfig for frontend to talk with backend

---
+++ howudoin-master/src/main/java/com/nulab/howudonodocker/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.nulab.howudonodocker.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8081") // React development server
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
